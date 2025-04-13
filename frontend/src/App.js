b17b499436382c3bb0969e10ab98aae93cf75d0d
Hector Acosta <1Hector.AC@gmail.com>
2025-04-13 12:55:47 -0700

    Add Home page and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import MyNavbar from './components/MyNavbar';
+import Home from './pages/Home';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
-        <h1>Test</h1>
+        <MyNavbar />
+        <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+          </Routes>
+        </BrowserRouter>
       
     </div>
   );
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+
+const Home = () => {
+
+    return (
+        <div className='Home'>
+            <h1>Home Page</h1>
+            <p>Insert Description of website comming soon</p>
+        </div>
+
+    );
+};
+export default Home;
