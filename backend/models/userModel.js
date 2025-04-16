638d8178a4b9156f1f73d82bf32068135e2a0898
Hector Acosta <1Hector.AC@gmail.com>
2025-04-16 13:43:07 -0700

    Fix spelling error.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -44,7 +44,7 @@ userSchema.statics.signup = async function(email,password){
     }
 
     // Encrypt password
-    const salt = await bcrypt.getSalt(10);
+    const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password,salt);
     
     const user = await this.create({email, password:hash});
@@ -52,4 +52,4 @@ userSchema.statics.signup = async function(email,password){
 }
 
 
-module.exports = mongoose.Model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
