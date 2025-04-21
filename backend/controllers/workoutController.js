08103a5294ba0b892229b314c916f4dbf447082d
Hector Acosta <1Hector.AC@gmail.com>
2025-04-21 13:37:38 -0700

    Added logic to get/add workout(s) in backend workoutController

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,12 +1,33 @@
+const Workout = require('../models/workoutModel');
 
 // Get all workouts owned by a certain user from db.
-const get_workouts = (req,res) => {
-    res.status(200).json({mssg:'get workouts'});
+const get_workouts = async (req,res) => {
+    try{
+       
+        const workouts = await Workout.find({});
+        res.status(200).json(workouts);
+        
+    }catch(err){
+        console.log('Error trying to get workouts')
+        res.status(400).json({error:err.message});
+    }
+    
 }
 
 // Add workout owned by user from db.
-const add_workout = (req,res) =>{
-    res.status(200).json({mssg:'add workouts'});
+const add_workout = async (req,res) =>{
+    const data = req.body;
+
+    // add empty field validation
+
+    try{
+        const workout = await Workout.create({...data});
+        res.status(200).json(workout);
+    } catch(err){
+        console.log("Error Trying to add workout");
+        res.status(400).json({errror:err.message});
+    }
+    
 }
 
 // Delete a workout owned by a user from db.
