3dd322db10068aaa54a5d52d1a657cebaa3bb303
Hector Acosta <1Hector.AC@gmail.com>
2025-04-25 13:36:21 -0700

    Edit form layout in MyWorkouts page

--- frontend/src/pages/MyWorkouts.js
+++ frontend/src/pages/MyWorkouts.js
@@ -10,26 +10,26 @@ const MyWorkouts = () => {
     const [weekday, setWeekday] = useState('monday');
     const [bodyPart, setBodyPart] = useState('');
 
-    const [error,setError] = useState(null);
+    const [error, setError] = useState(null);
     const [emptyFields, setEmptyFields] = useState([]);
-    
+
 
     // Handle submit for adding a workout.
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const workout = {title, reps,sets,weekday,bodyPart,user_id:'abc123'};
+        const workout = { title, reps, sets, weekday, bodyPart, user_id: 'abc123' };
         const response = await fetch('/api/workout/workout', {
-            method:'POST',
+            method: 'POST',
             body: JSON.stringify(workout),
-            headers:{'Content-Type':'application/json'}
+            headers: { 'Content-Type': 'application/json' }
         })
         const json = await response.json();
 
-        if(!response.ok){
+        if (!response.ok) {
             setError(json.error);
             setEmptyFields(json.emptyFields);
         }
-        if(response.ok){
+        if (response.ok) {
             // Clear fields.
             setTitle('');
             setReps('');
@@ -39,8 +39,6 @@ const MyWorkouts = () => {
             setError(null);
             setEmptyFields([]);
         }
-        
-
     }
 
     // Setup getting workouts in useEffect.
@@ -48,12 +46,12 @@ const MyWorkouts = () => {
         const fetchWorkouts = async () => {
             const response = await fetch('/api/workout/workouts');
             const json = await response.json();
-            if(response.ok){
+            if (response.ok) {
                 setWorkouts(json);
             }
         }
         fetchWorkouts();
-    },[]);
+    }, []);
 
     return (
         <div>
@@ -61,27 +59,46 @@ const MyWorkouts = () => {
 
             <hr />
             <form onSubmit={handleSubmit}>
+                <div className="form-group">
                 <h3>Add a Workout</h3>
-                <label>Title</label>
-                <input type='text' className={emptyFields.includes('title')?'errorTextBox':''} value={title} onChange={(e) => setTitle(e.target.value)} />
-                <label>reps</label>
-                <input type='number' className={emptyFields.includes('reps')?'errorTextBox':''} value={reps} onChange={(e) => setReps(e.target.value)} />
-                <label>sets</label>
-                <input type='number' className={emptyFields.includes('sets')?'errorTextBox':''} value={sets} onChange={(e) => setSets(e.target.value)} />
-                <label>Day</label>
-                <select value={weekday} onChange={(e) => setWeekday(e.target.value)}>
-                    <option value='monday'>Monday</option>
-                    <option value='tuesday'>Tuesday</option>
-                    <option value='wednesday'>Wednesday</option>
-                    <option value='thursday'>Thursday</option>
-                    <option value='friday'>Friday</option>
-                    <option value='saturday'>Saturday</option>
-                    <option value='sunday'>Sunday</option>
-                </select>
-                <label>BodyPart</label>
-                <input className={emptyFields.includes('bodyPart')?'errorTextBox':''} type='text' value={bodyPart} onChange={(e) => setBodyPart(e.target.value)} />
-                <button>Submit</button>
+                <div className='row'>
+                    <div className="col-md-2 offset-md-1">
+                        <label>Title</label>
+                        <input type='text' className={emptyFields.includes('title') ? 'errorTextBox' : 'form-control'} value={title} onChange={(e) => setTitle(e.target.value)} />
+                    </div>
+                    <div className="col-md-2">
+                        <label>reps</label>
+                        <input type='number' className={emptyFields.includes('reps') ? 'errorTextBox' : 'form-control'} value={reps} onChange={(e) => setReps(e.target.value)} />
+                    </div>
+                    <div className='col-md-2'>
+                        <label>sets</label>
+                        <input type='number' className={emptyFields.includes('sets') ? 'errorTextBox' : 'form-control'} value={sets} onChange={(e) => setSets(e.target.value)} />
+
+                    </div>
+                    <div className='col-md-2'>
+                        <label>Day</label>
+                        <select className="form-control" value={weekday} onChange={(e) => setWeekday(e.target.value)}>
+                            <option value='monday'>Monday</option>
+                            <option value='tuesday'>Tuesday</option>
+                            <option value='wednesday'>Wednesday</option>
+                            <option value='thursday'>Thursday</option>
+                            <option value='friday'>Friday</option>
+                            <option value='saturday'>Saturday</option>
+                            <option value='sunday'>Sunday</option>
+                        </select>
+                    </div>
+
+                    <div className="col-md-2">
+                        <label>BodyPart</label>
+                        <input className={emptyFields.includes('bodyPart') ? 'errorTextBox' : 'form-control'} type='text' value={bodyPart} onChange={(e) => setBodyPart(e.target.value)} />
+
+                    </div>
+                </div>
+                <br />
+                <button className="btn btn-primary">Submit</button>
                 {error && <div className="error">{error}</div>}
+                </div>
+                
             </form>
             <div>
 
