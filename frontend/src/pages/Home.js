dc53f1dec7e40fe359dc5042c0231705cc039d68
Hector Acosta <1Hector.AC@gmail.com>
2025-04-26 12:51:40 -0700

    Edit home page text and style.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,8 +3,19 @@ const Home = () => {
 
     return (
         <div className='Home'>
-            <h1>Home Page</h1>
-            <p>Insert Description of website comming soon</p>
+            <div className="headerPanel">
+                <h1>Welcome to Workout Calender!</h1>
+                <p>Create a weekly workout routine with our simple to use website.</p>
+            </div>
+            <div class='centerPanel'>
+                <h3>How to?</h3>
+                <p>- Make an account/log in</p>
+                <p>- Go to MyWorkouts page</p>
+                <p>- Fill out the form at the top to add a workout</p>
+                <p>- Workouts will be displays below the form</p>
+                <p>- Done</p>
+            </div>
+            
         </div>
 
     );
