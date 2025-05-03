0fc3d5b16b95775b0e8420ea798b9ad9dbfbe1c6
Hector Acosta <1Hector.AC@gmail.com>
2025-05-03 13:35:49 -0700

    Added delete workouts to MyWorkouts page and minor css changes

--- frontend/src/pages/MyWorkouts.js
+++ frontend/src/pages/MyWorkouts.js
@@ -1,5 +1,42 @@
 import { useEffect, useState } from "react";
 
+// React component that creates a table for workout data.
+const WorkoutTable = (props) => {
+    return (
+        <div className="workoutPanel">
+            <div className="row">
+                <div className="col-md-3">
+                    <h5 style={{ padding: '20px' }}>{props.title}</h5>
+                </div>
+                <div className="col-md-9">
+                    <table className='workoutTable table table-striped table-sm'>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Reps</th>
+                                <th>Sets</th>
+                                <th>Body Part</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.workoutData.map((w) => (
+                                <tr key={w._id}>
+                                    <td>{w.title}</td>
+                                    <td>{w.reps}</td>
+                                    <td>{w.sets}</td>
+                                    <td>{w.bodyPart}</td>
+                                    <td><button className="btn btn-primary" onClick={() => props.handleDeleteSubmit(w._id, w.weekday)}>Delete</button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 const MyWorkouts = () => {
     const [workouts, setWorkouts] = useState(null);
 
@@ -12,7 +49,6 @@ const MyWorkouts = () => {
 
     const [error, setError] = useState(null);
     const [emptyFields, setEmptyFields] = useState([]);
-
 
     // Handle submit for adding a workout.
     const handleSubmit = async (e) => {
@@ -38,8 +74,28 @@ const MyWorkouts = () => {
             setBodyPart('');
             setError(null);
             setEmptyFields([]);
+
+            // Edit workout state to reflect added workout.
+            let tempWorkouts = { ...workouts };
+            tempWorkouts[workout.weekday].push(workout);
+            setWorkouts(tempWorkouts);
         }
     }
+
+    // Handle delete of workouts.
+    const handleDeleteSubmit = async (id, weekday) => {
+        const response = await fetch('/api/workout/workout/' + id, {
+            method: 'DELETE'
+        });
+        //const json = await response.json();
+
+        if (response.ok) {
+            // Remove workout for local workouts state.
+            let tempWorkouts = { ...workouts };
+            tempWorkouts[weekday] = tempWorkouts[weekday].filter(i => i._id !== id);
+            setWorkouts(tempWorkouts);
+        }
+    };
 
     // Setup getting workouts in useEffect.
     useEffect(() => {
@@ -50,7 +106,7 @@ const MyWorkouts = () => {
                 //setWorkouts(json);
                 const predefinedCategories = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
-                const grouped = predefinedCategories.reduce((acc, weekday) => {
+                let grouped = predefinedCategories.reduce((acc, weekday) => {
                     acc[weekday] = json.filter(item => item.weekday === weekday);
                     return acc;
                 }, {});
@@ -61,51 +117,14 @@ const MyWorkouts = () => {
         fetchWorkouts();
     }, []);
 
-    // React component that creates a table for workout data.
-    const WorkoutTable = (props) => {
-        return (
-            <div className="workoutPanel">
-                <div className="row">
-                    <div className="col-md-3">
-                        <h5 style={{padding:'20px'}}>{props.title}</h5>
-                    </div>
-                    <div className="col-md-9">
-                        <table className='workoutTable table table-striped table-sm'>
-                            <thead>
-                                <tr>
-                                    <th>Title</th>
-                                    <th>Reps</th>
-                                    <th>Sets</th>
-                                    <th>Body Part</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {props.workoutData.map((w) => (
-                                    <tr key={w.title}>
-                                        <td >{w.title}</td>
-                                        <td> {w.reps}</td>
-                                        <td>{w.sets}</td>
-                                        <td>{w.bodyPart}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-
-
-            </div>
-
-        )
-    }
-
     return (
         <div>
-            <h1>My Workouts Page</h1>
-
+            <div className="headerPanel">
+                <h1>My Workouts Page</h1>
+            </div>
             <hr />
-            <form onSubmit={handleSubmit}>
+
+            <form onSubmit={handleSubmit} className="formPanel">
                 <div className="form-group">
                     <h3>Add a Workout</h3>
                     <div className='row'>
@@ -120,7 +139,6 @@ const MyWorkouts = () => {
                         <div className='col-md-2'>
                             <label>sets</label>
                             <input type='number' className={emptyFields.includes('sets') ? 'errorTextBox' : 'form-control'} value={sets} onChange={(e) => setSets(e.target.value)} />
-
                         </div>
                         <div className='col-md-2'>
                             <label>Day</label>
@@ -134,37 +152,35 @@ const MyWorkouts = () => {
                                 <option value='sunday'>Sunday</option>
                             </select>
                         </div>
-
                         <div className="col-md-2">
                             <label>BodyPart</label>
                             <input className={emptyFields.includes('bodyPart') ? 'errorTextBox' : 'form-control'} type='text' value={bodyPart} onChange={(e) => setBodyPart(e.target.value)} />
-
                         </div>
                     </div>
                     <br />
                     <button className="btn btn-primary">Submit</button>
                     {error && <div className="error">{error}</div>}
                 </div>
-
             </form>
-            <div>
 
+            <div>
                 <hr />
                 <h3>My Workouts Calender</h3>
 
-                {workouts && <WorkoutTable workoutData={workouts.monday} title='Monday' />}
+                {workouts && <WorkoutTable workoutData={workouts.monday} title='Monday' handleDeleteSubmit={handleDeleteSubmit} />}
 
-                {workouts && <WorkoutTable workoutData={workouts.tuesday} title='Tuesday' />}
+                {workouts && <WorkoutTable workoutData={workouts.tuesday} title='Tuesday' handleDeleteSubmit={handleDeleteSubmit} />}
 
-                {workouts && <WorkoutTable workoutData={workouts.wednesday} title='Wednesday' />}
+                {workouts && <WorkoutTable workoutData={workouts.wednesday} title='Wednesday' handleDeleteSubmit={handleDeleteSubmit} />}
 
-                {workouts && <WorkoutTable workoutData={workouts.thursday} title='Thursday' />}
+                {workouts && <WorkoutTable workoutData={workouts.thursday} title='Thursday' handleDeleteSubmit={handleDeleteSubmit} />}
 
-                {workouts && <WorkoutTable workoutData={workouts.friday} title='Friday' />}
+                {workouts && <WorkoutTable workoutData={workouts.friday} title='Friday' handleDeleteSubmit={handleDeleteSubmit} />}
 
-                {workouts && <WorkoutTable workoutData={workouts.saturday} title='Saturday' />}
+                {workouts && <WorkoutTable workoutData={workouts.saturday} title='Saturday' handleDeleteSubmit={handleDeleteSubmit} />}
 
-                {workouts && <WorkoutTable workoutData={workouts.sunday} title='Sunday' />}
+                {workouts && <WorkoutTable workoutData={workouts.sunday} title='Sunday' handleDeleteSubmit={handleDeleteSubmit} />}
+
             </div>
         </div>
     )
