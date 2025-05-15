189e89e33f00681b08fe9791f957a86956fbc11a
Hector Acosta <1Hector.AC@gmail.com>
2025-05-15 14:00:05 -0700

    Edit styling of login and signup pages.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,27 +1,33 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
-    const [email,setEmail] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const {login, error, isLoading} = useLogin();
+    const { login, error, isLoading } = useLogin();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await login(email,password);
+        await login(email, password);
     }
     return (
-        
-            <form className='login' onSubmit={handleSubmit}>
-                <h1>Login</h1>
-                <label htmlFor="email">Email</label>
-                <input type="email" id='email' onChange={(e) => setEmail(e.target.value)} value={email}/>
-                <label htmlFor="password">Password</label>
-                <input type="password" id='password' onChange={(e) => setPassword(e.target.value)} value={password}/>
-                <button disabled={isLoading}>Submit</button>
-                {error && <div className='error'>{error}</div>}
-            </form>
+        <div style={{ padding: '20px' }}>
+            <div className='formStyling'>
+                <form className='login' onSubmit={handleSubmit}>
+                    <h2>Login</h2>
+                    <hr />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className='form-control' id='email' onChange={(e) => setEmail(e.target.value)} value={email} />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className='form-control' id='password' onChange={(e) => setPassword(e.target.value)} value={password} />
+                    <br />
+                    <button className='btn btn-primary' disabled={isLoading}>Submit</button>
+                    {error && <div className='error'>{error}</div>}
+                </form>
+
+            </div>
+        </div>
 
     );
 };
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,10 +1,10 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { useSignup } from '../hooks/useSignup';
 
 const Signup = () => {
-    const [email,setEmail] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const {signup, error, isLoading} = useSignup();
+    const { signup, error, isLoading } = useSignup();
 
 
     const handleSubmit = async (e) => {
@@ -12,16 +12,23 @@ const Signup = () => {
         await signup(email, password);
     }
     return (
-        
-            <form className='signup' onSubmit={handleSubmit}>
-                <h1>Signup</h1>
-                <label htmlFor="email">Email</label>
-                <input type="email" id='email' onChange={(e) => setEmail(e.target.value)} value={email}/>
-                <label htmlFor="password">Password</label>
-                <input type="password" id='password' onChange={(e) => setPassword(e.target.value)} value={password}/>
-                <button disabled={isLoading}>Submit</button>
-                {error && <div className='error'>{error}</div>}
-            </form>
+        <div style={{ padding: '20px' }}>
+            <div className='formStyling'>
+                <form className='signup' onSubmit={handleSubmit}>
+                    <h2>Signup</h2>
+                    <hr />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="form-control" id='email' onChange={(e) => setEmail(e.target.value)} value={email} />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className='form-control' id='password' onChange={(e) => setPassword(e.target.value)} value={password} />
+                    <br />
+                    <button className='btn btn-primary' disabled={isLoading}>Submit</button>
+                    {error && <div className='error'>{error}</div>}
+                </form>
+            </div>
+        </div>
+
+
 
     );
 };
